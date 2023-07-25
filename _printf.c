108c42c69d6223c3d1aaefaec40f23a07816c75d
Kholoudelbatal <kholouudelbatal@gmail.com>
2023-07-25 07:28:33 -0700

    updates

--- _printf.c
+++ _printf.c
@@ -7,13 +7,22 @@ int put_char(char c);
  * @format: the string format
  * @...: number of variable arguments
  * Return: number of printed chars
-*/
+ */
 int _printf(const char *format, ...)
 {
 	int printed_chars = 0;
-	va_list args;
 
+	va_list args;
 	va_start(args, format);
+	
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		 return (-1);
+	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])	
+	{
+		return (-1);
+	}
 
 	while (*format)
 	{
@@ -21,83 +30,76 @@ int _printf(const char *format, ...)
 		{
 			format++;
 
-		switch (*format)
-		{
-
-			case 'c':
-				put_char((char)va_arg(args, int));
-				printed_chars++;
-				break;
-
-			case 's':
+			switch (*format)
 			{
-				int l;
-				char *st = va_arg(args, char *);
+				case 'c':
+					{
+						char c = (char)va_arg(args, int);
+						put_char(c);
+						printed_chars++;
+						break;
+					}
 
-				if (st == NULL)
-					st = "(NULL)";
+				case 's':
+					{
+						char *str = va_arg(args, char *);
+						if (str == NULL)
+							str = "(NULL)";
 
-				for (l = 0; st[l]; l++)
-				{
-					put_char(st[l]);
+						for (; *str; str++)
+						{
+							put_char(*str);
+							printed_chars++;
+						}
+						break;
+					}
+
+				case '%':
+					put_char('%');
 					printed_chars++;
-				}
-				break;
-
-			}
-
-			case '%':
-				put_char('%');
-				printed_chars++;
-				break;
-			case 'i':
-			case 'd':
-				{
-					int m = va_arg(args, int);
-					int x = 0;
-					int temp = m;
-
-					if (m < 0)
-					{
-						put_char('-');
-						printed_chars++;
-						m = -m;
-					}
-
-					do
-
-					{
-						x++;
-						temp /= 10;
-					}
-
-					while (temp);
-					temp = m;
-
-					do
-
-					{
-
-						int digit = temp % 10;
-
-						put_char('0' + digit);
-						printed_chars++;
-						temp /= 10;
-					} while (--x > 0);
 					break;
 
-			default:
+				case 'd':
+				case 'i':
+					{
+						int num = va_arg(args, int);
+						int digits = 0;
+						int temp = num;
 
-				put_char('%');
-				put_char(*format);
-				printed_chars += 2;
-				break;
-				}
+						if (num < 0)
+						{
+							put_char('-');
+							printed_chars++;
+							num = -num;
+						}
+
+						do
+						{
+							digits++;
+							temp /= 10;
+						} while (temp);
+
+						temp = num;
+						do
+						{
+							int digit = temp % 10;
+							put_char('0' + digit);
+							printed_chars++;
+							temp /= 10;
+						} while (--digits > 0);
+
+						break;
+					}
+
+				default:
+					put_char('%');
+					put_char(*format);
+					printed_chars += 2;
+					break;
 			}
 		}
 		else
 		{
-
 			put_char(*format);
 			printed_chars++;
 		}
@@ -107,17 +109,17 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 
-	return (printed_chars);
-
+	return printed_chars;
 }
+
 /**
  * put_char - prints char c to stdout
  * @c: the character to print
  * Return: On success 1
- *              On error, -1 is returned, and errno iss set appropriatly
-*/
+ *              On error, -1 is returned, and errno is set appropriately
+ */
 int put_char(char c)
 {
-	return (write(1, &c, 1));
+	return write(1, &c, 1);
 }
 
